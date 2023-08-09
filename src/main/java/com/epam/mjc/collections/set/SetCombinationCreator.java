edd14707aa535ef1b9e74183639befcd1fa6c112
MirrorwindAbyss <abdumalikunusov06@gmail.com>
2023-08-09 11:51:50 +0500

    Done

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -7,7 +7,6 @@ public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
         Set<String> s = new HashSet<>(firstSet);
         s.retainAll(secondSet);
-        s.retainAll(thirdSet);
         Set<String> temp = new HashSet<>(thirdSet);
         temp.removeAll(firstSet);
         temp.removeAll(secondSet);
